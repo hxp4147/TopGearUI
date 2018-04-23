43349ca99177133e617117efa171e3b2f8990bb7
kibates7 <bates.kiara@gmail.com>
2018-04-23 15:44:19 -0500

    Website

--- main/src/app/app.module.ts
+++ main/src/app/app.module.ts
@@ -2,10 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
+import { BannerComponent } from './banner/banner.component';
+import { RatingsComponent } from './ratings/ratings.component';
+import { StrengthsComponent } from './strengths/strengths.component';
+import { TableComponent } from './table/table.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    BannerComponent,
+    RatingsComponent,
+    StrengthsComponent,
+    TableComponent
   ],
   imports: [
     BrowserModule
