e8d3bef96f57fdf48a70ae69ecda340b055903ab
Reginald Todd Randolph Jr <reginaldrandolph@my.unt.edu>
2018-04-24 14:48:37 -0500

    updates

--- main/src/app/app.module.ts
+++ main/src/app/app.module.ts
@@ -2,10 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { BannerComponent } from './banner/banner.component';
+import { RatingsComponent } from './ratings/ratings.component';
+import { StrengthsComponent } from './strengths/strengths.component';
+import { TableComponent } from './table/table.component';
+import { DetailPanelComponent } from './detail-panel/detail-panel.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    BannerComponent,
+    RatingsComponent,
+    StrengthsComponent,
+    TableComponent,
+    DetailPanelComponent
   ],
   imports: [
     BrowserModule
---
+++ main/src/app/detail-panel/detail-panel.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-detail-panel',
+  templateUrl: './detail-panel.component.html',
+  styleUrls: ['./detail-panel.component.css']
+})
+export class DetailPanelComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
